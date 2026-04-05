f30f6c5346c2c8bad09091d5f374783484c621da
ajaysi <ajay.calsoft@gmail.com>
2026-04-05 07:17:40 +0530

    debug(frontend): log gating at PodcastMaker/ui/index.ts

--- frontend/src/components/PodcastMaker/ui/index.ts
+++ frontend/src/components/PodcastMaker/ui/index.ts
@@ -2,3 +2,5 @@ export { GlassyCard, glassyCardSx } from "./GlassyCard";
 export { PrimaryButton } from "./PrimaryButton";
 export { SecondaryButton } from "./SecondaryButton";
 
+import { shouldSkipOnboarding } from '../../../utils/demoMode';
+console.log('PodcastMaker UI index loaded. shouldSkipOnboarding=', shouldSkipOnboarding());
