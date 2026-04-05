88deabb9fcd2dbc94c9ae314b48df061bb425559
ajaysi <ajay.calsoft@gmail.com>
2026-04-05 07:22:53 +0530

    fix(frontend): satisfy ESLint by moving import to top and removing module-time log

--- frontend/src/components/PodcastMaker/ui/index.ts
+++ frontend/src/components/PodcastMaker/ui/index.ts
@@ -1,6 +1,4 @@
+import { shouldSkipOnboarding } from '../../../utils/demoMode';
 export { GlassyCard, glassyCardSx } from "./GlassyCard";
 export { PrimaryButton } from "./PrimaryButton";
 export { SecondaryButton } from "./SecondaryButton";
-
-import { shouldSkipOnboarding } from '../../../utils/demoMode';
-console.log('PodcastMaker UI index loaded. shouldSkipOnboarding=', shouldSkipOnboarding());
